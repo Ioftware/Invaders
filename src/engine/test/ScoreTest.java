ee74c78cc4f938d83b4d9bda7c82e1768ca84d1f
rasauq1122 <rasauq1122@gmail.com>
2020-12-14 11:30:13 +0900

    Test: ScoreTest 작성
    JIRA ISSUE KEY : IN-64

--- src/engine/test/ScoreTest.java
+++ src/engine/test/ScoreTest.java
@@ -2,28 +2,43 @@ package engine.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import engine.Score;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ScoreTest {
 
+	Score score;
+
+	@BeforeEach
+	void setup() {
+		score = new Score("PJS", 100);
+	}
+
 	@Test
 	void testScore() {
-		fail("Not yet implemented");
+		try {
+			new Score("PJS", 100);
+		}
+		catch (Exception e) {
+			fail();
+		}
 	}
 
 	@Test
 	void testGetName() {
-		fail("Not yet implemented");
+		assertTrue(score.getName() == "PJS");
 	}
 
 	@Test
 	void testGetScore() {
-		fail("Not yet implemented");
+		assertTrue(score.getScore() == 100);
 	}
 
 	@Test
 	void testCompareTo() {
-		fail("Not yet implemented");
+		Score score_compare = new Score("HHW", 101);
+		assertTrue(score.compareTo(score_compare) == 1);
 	}
 
 }
